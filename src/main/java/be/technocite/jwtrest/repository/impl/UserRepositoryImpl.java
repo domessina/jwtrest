04dc0567a679c7cbadfab370e62b6fb7b933eb25
9805_devops (Teacher) <t_9805_devops@PEDAGOGIQUE.LAN>
2019-09-16 13:40:12 +0200

    first commit

---
+++ src/main/java/be/technocite/jwtrest/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,34 @@
+package be.technocite.jwtrest.repository.impl;
+
+import be.technocite.jwtrest.model.User;
+import be.technocite.jwtrest.repository.UserRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    private ArrayList<User> users = newArrayList();
+
+    @Override
+    public User findByEmail(String email) {
+        return users.stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public User save(User user) {
+        if(findByEmail(user.getEmail()) == null) {
+            users.add(user);
+        }else {
+            users.remove(user);
+            users.add(user);
+        }
+        return findByEmail(user.getEmail());
+    }
+}
